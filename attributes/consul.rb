7e7e0c70081b1eb12287b8a9314acdacb404c965
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2016-12-10 19:13:55 -0800

    unset atlas token

--- attributes/consul.rb
+++ attributes/consul.rb
@@ -3,5 +3,5 @@ default.consul.service_mode = 'client'
 default.consul.enable_syslog = true
 default.consul.atlas_autojoin = true
 default.consul.atlas_cluster = 'default'
-default.consul.atlas_token = 'Uz2akkdryK84RqxbhyR-akbtxf9ox8Xob7fVT3hZL6KrN1dR7hHhxxg1z8ZqBRyqyj4'
+# default.consul.atlas_token
 
