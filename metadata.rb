0750a8072c9dcb84a7324e25f48501dd0db95a7e
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2023-03-07 07:43:51 +0000

    epel: only override on OL9

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name 'base'
-version '0.5.11'
+version '0.5.12'
 
 depends 'chef_client_updater'
 depends 'yum-epel'
--- recipes/epel.rb
+++ recipes/epel.rb
@@ -1,33 +1,20 @@
 include_recipe 'yum-epel'
 
-
-yum_repository 'ol9_developer_EPEL' do
-  repositoryid 'oracle-epel-ol9'
-  description 'Oracle Linux $releasever EPEL Packages for Development ($basearch)'
-
-  baseurl 'https://yum$ociregion.$ocidomain/repo/OracleLinux/OL9/developer/EPEL/$basearch/'
-  gpgkey 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle'
-  gpgcheck true
-  enabled true
+if platform?('oracle')
+  if node['platform_version'].to_i == 9
+    yum_repository 'epel' do
+      repositoryid 'epel'
+      description 'Oracle Linux $releasever EPEL Packages for Development ($basearch)'
+      baseurl 'https://yum$ociregion.$ocidomain/repo/OracleLinux/OL9/developer/EPEL/$basearch/'
+      mirrorlist nil
+      fastestmirror_enabled nil
+      gpgkey 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle'
+      gpgcheck true
+      enabled true
+    end
+  end
+  
+  file '/etc/yum.repo.d/oracle-epel-ol9.repo' do
+    action :delete
+  end
 end
-
-yum_repository 'epel' do
-  repositoryid 'epel'
-  description 'Oracle Linux $releasever EPEL Packages for Development ($basearch)'
-  # description 'Extra Packages for 9 - $basearch'
-
-  baseurl 'https://yum$ociregion.$ocidomain/repo/OracleLinux/OL9/developer/EPEL/$basearch/'
-  # mirrorlist 'https://mirrors.fedoraproject.org/mirrorlist?repo=epel-9&arch=$basearch'
-  mirrorlist nil
-  gpgkey 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle'
-  # gpgkey 'https://download.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-9'
-  gpgcheck true
-  enabled true
-  # fastestmirror_enabled=0
-end
-
-# Do NOT modify this file by hand.
-
-
-# [ol9_developer_EPEL]
-# name=Oracle Linux $releasever EPEL Packages for Development ($basearch)
