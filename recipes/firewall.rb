12f06b435a8713a5eb3d8bb5f591c3fb7029ef86
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2023-03-21 22:28:02 +0000

    firewall: disable on unsuported systems

--- recipes/firewall.rb
+++ recipes/firewall.rb
@@ -1,10 +1,16 @@
-firewalld 'default'
+if  platform_family?('debian') or platform_family?('rhel')
+  firewalld 'default'
 
-firewalld_zone 'public' do
-  interfaces node['network']['interfaces'].keys
-  services [
-    'dhcpv6-client',
-    'ssh',
-    'mosh'
-  ]
+  firewalld_zone 'public' do
+    interfaces node['network']['interfaces'].keys
+    services [
+      'dhcpv6-client',
+      'ssh',
+      'mosh'
+    ]
+  end
+else
+  # TODO: PF on OpenBSD, macOS, FreeBSD, Solaris
+  # TODO: IPF on Illumos
+  Chef::Log.warn "Firewall not configured on platform_family:#{node[:platform_family]}"
 end
