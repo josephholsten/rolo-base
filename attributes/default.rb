38a174c4b41e9d8395cf1ae6e0ff642bd1394848
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2023-04-20 04:37:09 +0000

    rsyslog.group: usa root_group

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,6 +6,7 @@ default["fail2ban"]["logtarget"] = 'SYSLOG'
 default["yum"]["epel"]["enabled"] = true
 
 default['rsyslog']['config_files']['group'] = node['root_group']
+default['rsyslog']['group'] = node['root_group']
 
 # TODO: https://www.papertrail.com/help/encrypting-remote-syslog-with-tls-ssl/#rsyslog
 # Required: default["rsyslog"]["custom_remote"]
