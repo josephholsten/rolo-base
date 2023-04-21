659013cb70d79b3717c3aea71b0bc1e82a87dd09
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2023-04-21 02:51:38 +0000

    rsyslog: usa service_name: rsyslogd in freebsd

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,13 @@ default["fail2ban"]["logtarget"] = 'SYSLOG'
 
 default["yum"]["epel"]["enabled"] = true
 
-default['rsyslog']['config_files']['group'] = node['root_group']
-default['rsyslog']['group'] = node['root_group']
+# TODO: patch rsyslog cookbook upstream
+if platform_family?('freebsd')
+  default['rsyslog']['config_files']['group'] = node['root_group']
+  default['rsyslog']['group'] = node['root_group']
+  default['rsyslog']['dir_group'] = node['root_group']
+  default['rsyslog']['service_name'] = 'rsyslogd'
+end
 
 # TODO: https://www.papertrail.com/help/encrypting-remote-syslog-with-tls-ssl/#rsyslog
 # Required: default["rsyslog"]["custom_remote"]
