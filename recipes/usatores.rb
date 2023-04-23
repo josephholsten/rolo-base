535ba2fd61e6b3f183df2340253a778e5da58bb7
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2023-04-23 20:34:31 +0000

    usatores: ssh: adde zohre, sh: usa ksh93 en freebsd

--- recipes/usatores.rb
+++ recipes/usatores.rb
@@ -5,12 +5,21 @@ end
 
 user_name    = 'j'
 user_comment = 'Joseph Anthony Pasquale Holsten'
-user_shell   = '/usr/bin/ksh'
+user_shell   = case node['platform_version']
+when 'rhel'
+  '/bin/ksh'
+when 'freebsd'
+  '/usr/local/bin/ksh93'
+else
+  # reasonable default, be sure to add entry above for supported platforms
+  '/bin/ksh'
+end
+
 user_home    = "/home/#{user_name}"
 user_ssh_keys = [
   'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINr+NtScZF5bGoEVD/S3PZgQEY3DKJlSkdeZUspuBRIn isha',
   'ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBP1QFEyvZ5VDRmDukmlgiL+8b27S1DJYY4kfgOhTvLrioN+DlJyBC5LhOiZPytLB/grfK/ETKzGhsZfLSPaiR4c= quaderno:blink',
-  'ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNGF1GKBRwA+y4ulkn6a6m6xSQjWpGqz6uOIwc+JibCVqKrtxZbrQqF3tnimGZJ7mOqWfUlgv3qv8pjc3Vt1FEs= venusta:blink',
+  'ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAi45VJG07ufOPkWLD3Dnnmi1Bn9n34Mnx4X2pjKYx7qLEsClsDMbIMiqbPzSB5BgisQEyyVouefYsf7D23uNJ8= zohre:blink',
 ]
 
 user user_name do
